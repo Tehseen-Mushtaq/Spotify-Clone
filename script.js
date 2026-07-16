3658f6ad67931703288a049105f9974970590f6c
Tehseen <officialtehseen1@gmail.com>
2026-07-16 13:20:56 +0530

    add json file for songs

--- script.js
+++ script.js
@@ -1,27 +1,11 @@
 async function getsongs() {
 
-    let a = await fetch("http://127.0.0.1:3000/songs/")
-    let response = await a.text()
-    // console.log(response)
-    let div = document.createElement("div")
-    div.innerHTML = response
-    let as = div.getElementsByTagName("a")
+    let a = await fetch("songs/songs.json")
+    let response = await a.json()
+    console.log(response)
+    
 
-
-    let songs = []
-
-    for (let index = 0; index < as.length; index++) {
-        const element = as[index];
-
-
-        if (element.href.endsWith("mp3")) {
-            songs.push(element.href)
-
-
-        }
-    }
-
-    return songs
+    return response
 }
 
 async function main() {
@@ -35,7 +19,7 @@ async function main() {
 
 
     for (const song of songs) {
-        songul.innerHTML = songul.innerHTML + `<li><img class ="invert" src="music-3-stroke-rounded.svg"> ${song.replaceAll("%20", "").replaceAll("#", "%20").replaceAll("%20", "").split("songs%5C")[1]}</li>`
+        songul.innerHTML = songul.innerHTML + `<li><img class ="invert" src="music-3-stroke-rounded.svg"> ${song}</li>`
         console.log(song)
     }
 
